4f64d9d594e6ff69a8efc7a0f856c6da9ae2dd02
jasetchell09 <98408044+jasetchell09@users.noreply.github.com>
2022-03-10 10:29:48 -0600

    Add files via upload

---
+++ Scripts/keepalive.ps1
@@ -0,0 +1,7 @@
+﻿#Script to keep session live from azure to aws.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+While ($true)
+{
+Test-NetConnection 8.8.8.8 -InformationLevel Quiet
+Start-Sleep -Seconds 5
+}
